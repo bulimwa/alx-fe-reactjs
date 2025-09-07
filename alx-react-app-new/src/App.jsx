3cb46e41de00711d91ba31b7e5b948df4feca70c
Ephantus Bulimwa <bulimwawanyoike@gmail.com>
2025-09-07 12:48:20 +0300

    App.jsx

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,12 @@
+eturn (
+    <div>
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
