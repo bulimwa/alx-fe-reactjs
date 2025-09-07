750eeb3e743ab23d109a4a67ca443f402bd6f51a
Ephantus Bulimwa <bulimwawanyoike@gmail.com>
2025-09-07 23:24:32 +0300

    another commit

--- my-company/src/Navbar.jsx
+++ my-company/src/Navbar.jsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
-    return (
-        <nav style={{ backgroundColor: 'navy', padding: '1rem' }}>
-            <Link to="/" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>Home</Link>
-            <Link to="/about" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>About</Link>
-            <Link to="/services" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>Services</Link>
-            <Link to="/contact" style={{ color: 'white', margin: '0 1rem', textDecoration: 'none' }}>Contact</Link>
-        </nav>
-    );
+  return (
+    <nav style={{ backgroundColor: '#f4f4f4', padding: '10px' }}>
+      <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
+      <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
+      <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '0 10px' }}>Contact</Link>
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
