133ad6d5c6f740301f80b37708e23834398fbb12
Ephantus Bulimwa <bulimwawanyoike@gmail.com>
2025-09-21 16:53:35 +0300

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/components/githubService.js
+++ github-user-search/src/components/githubService.js
@@ -3,9 +3,17 @@ import axios from 'axios';
 export const fetchAdvancedUsers = async (username, location, minRepos) => {
   let query = '';
 
-  if (username) query += `${username} `;
-  if (location) query += `location:${location} `;
-  if (minRepos) query += `repos:>${minRepos}`;
+  if (username) {
+    query += `${username} `;
+  }
+
+  if (location) {
+    query += `location:${location} `;
+  }
+
+  if (minRepos) {
+    query += `repos:>${minRepos}`;
+  }
 
   const url = `https://api.github.com/search/users?q=${encodeURIComponent(query.trim())}`;
   const response = await axios.get(url);
