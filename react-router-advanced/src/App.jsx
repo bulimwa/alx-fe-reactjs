c524027a7a54d45b447906c1039ca44478215c28
Ephantus Bulimwa <bulimwawanyoike@gmail.com>
2025-10-12 15:03:18 +0300

    Advanced Routing in React with React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,15 +1,36 @@
-import ProfileDetails from './pages/ProfileDetails';
-import ProfileSettings from './pages/ProfileSettings';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import BlogPost from './pages/BlogPost';
+import ProtectedRoute from './routes/ProtectedRoute';
+import Profile from './components/Profile';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
 
-// Inside <Route path="/profile/*" ...>
-<Route
-  path="/profile/*"
-  element={
-    <ProtectedRoute>
-      <Profile />
-    </ProtectedRoute>
-  }
->
-  <Route path="details" element={<ProfileDetails />} />
-  <Route path="settings" element={<ProfileSettings />} />
-</Route>
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/profile">Profile</Link> | <Link to="/blog/42">Blog Post</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
