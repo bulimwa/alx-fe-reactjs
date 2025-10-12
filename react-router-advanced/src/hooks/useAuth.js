52627ff6c6b5446190a8bd2ddf21c41c38dff487
Ephantus Bulimwa <bulimwawanyoike@gmail.com>
2025-10-12 15:35:48 +0300

    Fix: add useAuth hook and use in ProtectedRoute

---
+++ react-router-advanced/src/hooks/useAuth.js
@@ -0,0 +1,7 @@
+function useAuth() {
+  // Simulate authentication status
+  const isAuthenticated = false; // Change to true to simulate logged-in user
+  return { isAuthenticated };
+}
+
+export default useAuth;
